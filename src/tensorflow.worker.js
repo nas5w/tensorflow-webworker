06c42b7baef601a8f3991c718c7a22bc81fea584
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2019-06-26 19:37:28 -0400

    Update tf import

--- src/tensorflow.worker.js
+++ src/tensorflow.worker.js
@@ -1,13 +1,15 @@
-import { sequential, layers, randomNormal } from "@tensorflow/tfjs";
+import * as tf from "@tensorflow/tfjs";
 
 onmessage = () => {
-  const model = sequential();
-  model.add(layers.dense({ units: 100, activation: "relu", inputShape: [10] }));
-  model.add(layers.dense({ units: 1, activation: "linear" }));
+  const model = tf.sequential();
+  model.add(
+    tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] })
+  );
+  model.add(tf.layers.dense({ units: 1, activation: "linear" }));
   model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
 
-  const xs = randomNormal([100, 10]);
-  const ys = randomNormal([100, 1]);
+  const xs = tf.randomNormal([100, 10]);
+  const ys = tf.randomNormal([100, 1]);
 
   model.fit(xs, ys, {
     epochs: 100,
