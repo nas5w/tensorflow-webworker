c3eb6f07b4b1a680ad39d949d1fe1debe1dbebd7
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2019-06-26 20:16:33 -0400

    Add logging

--- src/tensorflow.worker.js
+++ src/tensorflow.worker.js
@@ -2,15 +2,19 @@ import * as tf from "@tensorflow/tfjs";
 
 onmessage = () => {
   try {
+    postMessage("1");
     const model = tf.sequential();
     model.add(
       tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] })
     );
+    postMessage("2");
     model.add(tf.layers.dense({ units: 1, activation: "linear" }));
     model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
+    postMessage("3");
 
     const xs = tf.randomNormal([100, 10]);
     const ys = tf.randomNormal([100, 1]);
+    postMessage("4");
 
     model.fit(xs, ys, {
       epochs: 100,
@@ -19,6 +23,7 @@ onmessage = () => {
           postMessage(`Epoch ${epoch}: loss = ${log.loss}`)
       }
     });
+    postMessage("5");
   } catch (e) {
     postMessage(e);
   }
