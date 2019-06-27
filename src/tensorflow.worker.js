e79a2bf171b1ba0152b24c47fe9c74eb88d585a3
Nick Scialli <7538045+nas5w@users.noreply.github.com>
2019-06-26 20:35:35 -0400

    Remove extraneous lodging

--- src/tensorflow.worker.js
+++ src/tensorflow.worker.js
@@ -2,19 +2,16 @@ import * as tf from "@tensorflow/tfjs";
 
 onmessage = () => {
   try {
-    postMessage("1");
+    postMessage(tf);
     const model = tf.sequential();
     model.add(
       tf.layers.dense({ units: 100, activation: "relu", inputShape: [10] })
     );
-    postMessage("2");
     model.add(tf.layers.dense({ units: 1, activation: "linear" }));
     model.compile({ optimizer: "sgd", loss: "meanSquaredError" });
-    postMessage("3");
 
     const xs = tf.randomNormal([100, 10]);
     const ys = tf.randomNormal([100, 1]);
-    postMessage("4");
 
     model.fit(xs, ys, {
       epochs: 100,
@@ -23,7 +20,6 @@ onmessage = () => {
           postMessage(`Epoch ${epoch}: loss = ${log.loss}`)
       }
     });
-    postMessage("5");
   } catch (e) {
     postMessage(e);
   }
